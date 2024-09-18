12ace9dfc7976003be62ebb791017c9e070c06b6
Assena-Emmanuel <assenaemmanuel3@gmail.com>
2024-09-18 12:20:37 +0000

    Premier commit

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Accueil',
+    to: { name: 'root' },
+    icon: { icon: 'ri-home-smile-2-line' },
+  },
+  {
+    title: 'Utilisateur',
+    to: { name: 'apps-user-list' },
+    icon: { icon: 'ri-user-line' },
+  },
+  // {
+  //   title: 'Utilisateur',
+  //   icon: { icon: 'ri-user-line' },
+  //   children: [
+  //     { title: 'Ajouter', to: 'apps-user-list' },
+  //     { title: 'Liste', to: { name: 'apps-user-view-id', params: { id: 21 } } },
+  //   ],
+  // },
+]
